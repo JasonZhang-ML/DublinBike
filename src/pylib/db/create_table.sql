7fe77abf9529c8d16c1800295004668d5cb24714
JasonZhang-ML <58825716+JasonZhang-ML@users.noreply.github.com>
2020-02-20 16:50:06 +0000

    New version of database table. 3 tables.

--- src/pylib/db/create_table.sql
+++ src/pylib/db/create_table.sql
@@ -1,29 +1,35 @@
 -- SQL used to created required tables;
 
-CREATE DATABASE `dbike`;
+CREATE DATABASE `dbbike`;
+
+USE `dbbike`;
 
 CREATE TABLE `station` (
-  `sid` int(11) NOT NULL AUTO_INCREMENT COMMENT 'pk',
-  `station_id` int(11) DEFAULT NULL ,
+  `sid` int(11) NOT NULL COMMENT 'the number of station unique',
   `station_name` varchar(40) DEFAULT NULL,
   `station_address` varchar(50) DEFAULT NULL COMMENT 'address',
   `lat` float(10,6) DEFAULT NULL COMMENT 'latitude',
   `lng` float(10,6) DEFAULT NULL COMMENT 'longitude',
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `availability` (
+  `aid` int(11) NOT NULL AUTO_INCREMENT COMMENT 'pk',
+  `station_id` int(11) DEFAULT NULL COMMENT 'foreign key from table station' ,
   `bike_stands` int(11) DEFAULT NULL COMMENT 'all stands',
   `ava_bikes` int(11) DEFAULT NULL COMMENT 'available bikes',
   `ava_stands` int(11) DEFAULT NULL COMMENT 'available stands',
   `status` ENUM('OPEN','CLOSED') DEFAULT 'OPEN',
-  `update` timestamp DEFAULT NULL COMMENT 'data update time in API',
+  `last_update` timestamp DEFAULT NULL COMMENT 'data update time in API',
   /*`modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'last modify time',*/
-  PRIMARY KEY (`sid`)
+  PRIMARY KEY (`aid`),
+  KEY `station_id_idx` (`station_id`),
+  CONSTRAINT `station_id` FOREIGN KEY (`station_id`) REFERENCES `station` (`sid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6000000 DEFAULT CHARSET=utf8mb4;
 
-
-CREATE TABLE `station_weather` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `station_id` int(11) DEFAULT NULL ,
-  `lat` float(10,6) DEFAULT NULL,
-  `lng` float(10,6) DEFAULT NULL,
+CREATE TABLE `weather` (
+  `wid` int(11) NOT NULL AUTO_INCREMENT,
+  `wstation_id` int(11) DEFAULT NULL COMMENT 'foreign key from table station sid',
   `weather_id` int(11) DEFAULT NULL COMMENT '3-digit id describe main weather',
   `temp` float(5,2) DEFAULT NULL COMMENT  'Celsius temperature',
   `feels_like` float(5,2) DEFAULT NULL COMMENT  'feels-like temperature in Celsius',
@@ -39,7 +45,8 @@ CREATE TABLE `station_weather` (
   `snow_1h` float(5,2) DEFAULT NULL COMMENT 'snow volume for the last 1 hour, mm',
   `snow_3h` float(5,2) DEFAULT NULL COMMENT 'snow volume for the last 3 hour, mm',
   `update_time`timestamp DEFAULT NULL COMMENT 'data update time in API',
-  `sid` int(10) DEFAULT NULL COMMENT 'foreign key of table station, unique',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`wid`),
+    KEY `wstation_id_idx` (`wstation_id`),
+  CONSTRAINT `wstation_id` FOREIGN KEY (`wstation_id`) REFERENCES `station` (`sid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3000000 DEFAULT CHARSET=utf8mb4;
 
